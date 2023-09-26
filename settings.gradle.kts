53333a794f9a29745f5a27230098d6d0793825c7
Stavitskiy Vladimir <vstavitskiy@corp.local>
2023-09-26 12:06:19 +0300

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "test-deploy"
+
+// Gradle plugin repositories
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+// Java project repositories
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        mavenCentral()
+    }
+}
